d1db0415d434f4838bb6e982f9a559b619676701
Paul Smith <a.paul.smith@gmail.com>
2017-08-17 09:59:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='bubble-blaster-game',
+      version='0.1',
+      description='Python Bubble Blaster Game',
+      url='https://github.com/apaulsmith/baptiste-python',
+      author='Baptiste',
+      author_email='none',
+      packages=['bubble-blaster-game'],
+      install_requires=[
+            'playsound'
+      ],
+      zip_safe=False)
